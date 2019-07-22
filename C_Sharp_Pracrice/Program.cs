12330c1be15a8a726db2b4a05ba813358204e8fb
falcon78_infodex <falconhawk@protonmail.com>
2019-07-22 12:33:25 +0900

    get and set methods

---
+++ C_Sharp_Pracrice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program
+{
+    class Genre
+    {
+        private string genre { get; set; }
+        private static void Main ()
+        {
+            Genre a = new Genre();
+            a.genre = "Hip-Hop";
+            Console.WriteLine(a.genre);
+        }
+    }
+}
